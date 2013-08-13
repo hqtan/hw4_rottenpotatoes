9c0139e5f4dcba2d515b89a6946932592be570c4
hqt <hanqing.tan@gmail.com>
2013-08-13 06:22:59 -0700

    add movies controller spec. spec taken from http://pastebin.com/L6FYWyV7

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'searching TMDb' do
+    it 'should call the model method that performs TMDb search'
+      pending "Need model method"
+    it 'should select the Search Results template for rendering'
+    it 'should make the TMDb search results available to that template'
+  end
+end
+
